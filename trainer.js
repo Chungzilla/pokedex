747fb8b6d8977d30fa2d72ef996e14561b62378a
Chungzilla <chung.jazmine@gmail.com>
2018-07-17 13:33:16 -0400

    moving nre pm instace to pm class

--- trainer.js
+++ trainer.js
@@ -18,28 +18,4 @@ class Trainer{
 
 
     //ADD method to add pokemon object to - 1 param 'pokeObj'
-    add (pokemonObj){
-
-        axios.get('https://pokeapi.co/api/v2/pokemon/' + pokemonObj).then((response) => {
-            // console.log(response.data);
-            let pokedata = response.data
-            console.log(pokedata)
-            let name = pokedata.name
-            let hp = pokedata.stats[5].base_stat
-            let defense = pokedata.stats[3].base_stat
-            let attack = pokedata.stats[4].base_stat
-            let pokeAbility = []
-            
-            //add abilities to string
-            pokedata.abilities.forEach(element =>
-                pokeAbility.push(element.ability.name)
-            )
-                // console.log(pokeAbility) 
-
-            //Create a new instance of pokemon
-            let newPokemon = new Pokemon(name, hp, defense, attack, pokeability)
-        });
-    }
-}
-
-}
+    add (pokemonObj)
