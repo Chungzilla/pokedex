619f83887c4407e6699096fa4597aea4a74800b4
Chungzilla <chung.jazmine@gmail.com>
2018-07-17 17:21:52 -0400

    revert pokemon js file

--- main.js
+++ main.js
@@ -1,19 +1,2 @@
 
 
-axios.get('https://pokeapi.co/api/v2/pokemon/1').then((response) => {
-    // console.log(response.data);
-    let pokedata = response.data
-    console.log(pokedata)
-    let name = pokedata.name
-    let hp = pokedata.stats[5].base_stat
-    let defense = pokedata.stats[3].base_stat
-    let attack = pokedata.stats[4].base_stat
-    let pokeAbility = []
-    
-    //add abilities to string
-    pokedata.abilities.forEach(element =>
-        pokeAbility.push(element.ability.name)
-    )
-        // console.log(pokeAbility) 
-    
-});
--- pokemon.js
+++ pokemon.js
@@ -1,22 +1,4 @@
-
-const baseUrl = `https://pokeapi.co/api/v2/pokemon/`;
-
-class Pokemon{
-    constructor(name, hp, defense, attack){
-        this.name = name;
-        this.hp = hp;
-        this.defense = defense;
-        this.attack = attack;
-        this.pokeAbility = []
-    
-        //add abilities to string
-        pokedata.abilities.forEach(element =>
-            pokeAbility.push(element.ability.name)
-        )
-    }
-}
-
-axios.get(`${baseUrl} + this.name`).then((response) => {
+axios.get('https://pokeapi.co/api/v2/pokemon/1').then((response) => {
     // console.log(response.data);
     let pokedata = response.data
     console.log(pokedata)
@@ -24,15 +6,26 @@ axios.get(`${baseUrl} + this.name`).then((response) => {
     let hp = pokedata.stats[5].base_stat
     let defense = pokedata.stats[3].base_stat
     let attack = pokedata.stats[4].base_stat
-        // console.log(pokeAbility) 
-
+    
+    let pokeAbility = []
+    //add abilities to string
+    pokedata.abilities.forEach(element =>
+        pokeAbility.push(element.ability.name)
+    ) // console.log(pokeAbility) 
+       
+    
 });
 
 
 
 
 
-
-
-
-
+class Pokemon{
+    constructor(name, hp, defense, attack){
+        this.name = name;
+        this.hp = hp;
+        this.defense = defense;
+        this.attack = attack;
+        this.abilities = []
+    }
+}
