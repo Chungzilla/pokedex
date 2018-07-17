06551e8b99328bc16b85d3cc58eee1eeb5933ab2
Chungzilla <chung.jazmine@gmail.com>
2018-07-16 22:08:23 -0400

    set up api call for bulbasaur

---
+++ pokemon.js
@@ -0,0 +1,9 @@
+class Pokemon{
+    constructor(name, hp, defense, attack){
+        this.name = name;
+        this.hp = hp;
+        this.defense = defense;
+        this.attack = attack;
+        this.abilities = []
+    }
+}
