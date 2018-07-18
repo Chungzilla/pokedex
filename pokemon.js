0fa6b4e9856ddb043d7bf147ddf4c218466f9d47
Chungzilla <chung.jazmine@gmail.com>
2018-07-18 17:16:53 -0400

    debugged to allow pokemon image to appear on click

--- pokemon.js
+++ pokemon.js
@@ -4,6 +4,7 @@
 // Meowth: https://pokeapi.co/api/v2/pokemon/52
 
 
+
 class Pokemon{
     constructor(name, hp, defense, attack, abilities){
         this.name = name;
@@ -30,18 +31,28 @@ axios.get('https://pokeapi.co/api/v2/pokemon/1').then((response) => {
     ) // console.log(pokeAbility) 
 
     let bulbasaur = new Pokemon(name, hp, defense, attack, pokeAbility)
-    console.log(bulbasaur)
+    console.log(bulbasaur);
 
-    kanye.add(bulbasaur)
+    kanye.add(bulbasaur);
 
     //Event listener to display pokemon on click
+    let pokeData = document.getElementById('pokedata');
 
-    let pokeball1 = document.getElementById('bulb-button')
+    let pokeball1 = document.getElementById('bulb-button');
 
     pokeball1.addEventListener('click', (e) => {
         e.preventDefault();
+        // alert('Yeaaa THIS WORKS!');
+//Creates new div container to display pokemon images
+        let pokeWindow = document.createElement('div');
+        pokeWindow.id = 'window';
+        let imgtag = document.createElement('img');
+        imgtag.classList.add('poke-img')
+        imgtag.src="bulbasaur.png";
 
-        alert('Yeaaa THIS WORKS!')
+        pokeWindow.append(imgtag);
+        pokeData.append(pokeWindow);
+    
     });
     
 });
