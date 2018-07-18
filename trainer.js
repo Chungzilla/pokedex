3fbffa64b032d23395b16b8ef60793d9b205ed16
Chungzilla <chung.jazmine@gmail.com>
2018-07-17 20:42:53 -0400

    debugged get metod in trainer class

--- trainer.js
+++ trainer.js
@@ -8,19 +8,21 @@ class Trainer{
     all (){
         console.log(this.pokemonObj);
     }
-
-//     //GET method to return 1 pokeObj - 1 param "name" -
-    get (name){
-        this.pokemonObj.find((element) => {
-            if(this.pokemonObj.includes(element)){
-                return element;
-        }else{
-            console.log('The trainer has not caught this pokemon yet!')}
-        });
-    }
-
 //     //ADD method to add pokemon object to - 1 param 'pokeObj'
     add (catchIt){
         this.pokemonObj.push(catchIt)
     }
+
+//     //GET method to return 1 pokeObj - 1 param "name" -
+    get (name){
+        this.pokemonObj.find(element => {
+            if(element.name === name){
+                console.log(element);
+            }else{
+                console.log('The trainer has not been caught this pokemon yet!')
+            }
+        });
+    }
+
+
 }
