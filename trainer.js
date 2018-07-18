285ee3d958e5fa0304399be5c495ab801e1bf88d
Chungzilla <chung.jazmine@gmail.com>
2018-07-17 20:23:47 -0400

    clened up punctuation in Trainer class

--- trainer.js
+++ trainer.js
@@ -1,21 +1,23 @@
 class Trainer{
     constructor(){
 //     //container object to store pokemon objects
-        this.pokemonObj = []
+        this.pokemonObj = [];
     }
 
 //     //ALL method to return an array of pokemon - no params-
     all (){
-        console.log(this.pokemonObj)
-      
-    };
+        console.log(this.pokemonObj);
+    }
 
 //     //GET method to return 1 pokeObj - 1 param "name" -
     get (name){
         this.pokemonObj.find((element) => {
-            return element.name == name
-        }
-    )}
+            if(this.pokemonObj.includes(element)){
+                return element;
+        }else{
+            console.log('The trainer has not caught this pokemon yet!')}
+        });
+    }
 
 //     //ADD method to add pokemon object to - 1 param 'pokeObj'
     add (catchIt){
